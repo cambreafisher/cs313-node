3ceb20a474ba651d9dcaa41f2ae1d5f42f1bb843
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-03-09 10:27:25 -0700

    inclass practice

--- classpractice/server.js
+++ classpractice/server.js
@@ -4,8 +4,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/playGame", handlePlayGame);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
+
+function handlePlayGame(req, res) {
+    console.log("playing game");
+
+    const playerWeapon = req.query.playerWeapon;
+    console.log("player weapon: " + playerWeapon);
+    
+    const cpuWeapon = "Scissors";
+    const winner = "Player";
+
+    const params = {playerWeapon: playerWeapon, cpuWeapon: cpuWeapon, winner: winner};
+    res.render("results", params);
+}
 
